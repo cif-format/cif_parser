cdc39a84271711d89c72e760a70c935f0933a3f2
mxtlrr <ruson0914@gmail.com>
2023-02-06 15:20:57 -0600

    feat: finish retrieving header from CIF file

--- src/main.c
+++ src/main.c
@@ -20,11 +20,12 @@ int main(int argc, char* argv[]){
   fclose(fp);
 
 
+
   // first let's check if the header is correct
-  unsigned short hdr = (buffer[0] << 8) | buffer[1]; // weird trick, but 0xfe 0xdb => 0xfedb
+  unsigned short magic_num = (buffer[0] << 8) | buffer[1]; // weird trick, but 0xfe 0xdb => 0xfedb
   int version = 0;
 
-  switch(hdr){
+  switch(magic_num){
     case v0:
       version = 0;
       break;
@@ -35,5 +36,29 @@ int main(int argc, char* argv[]){
       break;
   }
 
-  printf("CIF version %d detected\n", version);
+  // find width & height
+  cif_uint8_t width = buffer[2];
+  cif_uint8_t height = buffer[4]; // buffer[3] is reserved
+
+  cif_header_t _hdr = { .magic = magic_num,
+                        .width = width,
+                        .reserved = RESERVED,
+                        .height = height };
+  printf("CIF version %d detected (magic is 0x%x)\n", version, _hdr.magic);
+  printf("Dimensions of CIF file: %d by %d (width by height)\n", _hdr.width, _hdr.height);
+
+  // now lets display pixels
+  #ifdef __NON_FFREESTANDING
+    // some operating systems do not implement a scanf
+    
+
+    char m = 0x59;
+    printf("\nWould you like to display pixel data? (default=Y, Y/n) ");
+    scanf("%c", &m);
+    if(m != 0x59) return 0; // exit
+  #endif
+  
+  for(int k = 5; k < (sizeof(buffer)/sizeof(buffer[0])); k++){
+    // TODO
+  }
 }
